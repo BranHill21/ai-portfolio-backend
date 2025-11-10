287eb8c01f8496acecf95d1e82ecbb85ab2b82e3
BranHill21 <brandon.hill2@verizon.net>
2025-11-09 20:41:26 -0600

    adding webconfig to allow cors on local and netlify frontend

---
+++ src/main/java/com/brandonhill/portfolio_backend/WebConfig.java
@@ -0,0 +1,27 @@
+package com.brandonhill.portfolio_backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:3000",          // local React dev
+                            "stockfolioai.netlify.app" // deployed frontend
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
