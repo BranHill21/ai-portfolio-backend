2b8f023933bd62a546da051faf91c257ebbed773
BranHill21 <brandon.hill2@verizon.net>
2025-11-11 16:11:49 -0600

    added db init in main app

--- src/main/java/com/brandonhill/portfolio_backend/PortfolioBackendApplication.java
+++ src/main/java/com/brandonhill/portfolio_backend/PortfolioBackendApplication.java
@@ -1,12 +1,27 @@
 package com.brandonhill.portfolio_backend;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PortfolioBackendApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioBackendApplication.class, args);
-	}
+import com.brandonhill.portfolio_backend.config.MongoAtlasConnector;
 
+@SpringBootApplication
+public class PortfolioBackendApplication implements CommandLineRunner {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String dbName;
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioBackendApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        MongoAtlasConnector.init(mongoUri, dbName);
+    }
 }
