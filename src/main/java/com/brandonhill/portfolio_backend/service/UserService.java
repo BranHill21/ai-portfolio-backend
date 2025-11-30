69537c4904e7ffe9982a073b441ef31be24bb42f
BranHill21 <brandon.hill2@verizon.net>
2025-11-30 16:42:03 -0600

    implemented hashing of passwords and updated register and login functions

--- src/main/java/com/brandonhill/portfolio_backend/service/UserService.java
+++ src/main/java/com/brandonhill/portfolio_backend/service/UserService.java
@@ -3,6 +3,7 @@ package com.brandonhill.portfolio_backend.service;
 import com.brandonhill.portfolio_backend.model.User;
 import com.brandonhill.portfolio_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,8 +13,17 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public User register(User user) {
+    	String rawPassword = user.getPassword();
+        String hashedPassword = passwordEncoder.encode(rawPassword);
+        user.setPassword(hashedPassword);
     	Optional<User> testUser = userRepository.findByUsername(user.getUsername());
     	if(!testUser.equals(null)) {
     		return userRepository.save(user);
@@ -25,7 +35,7 @@ public class UserService {
 
     public Optional<User> login(String username, String password) {
         Optional<User> user = userRepository.findByUsername(username);
-        if (user.isPresent() && user.get().getPassword().equals(password)) {
+        if (user.isPresent() && passwordEncoder.matches(password, user.get().getPassword())) {
             return user;
         }
         return Optional.empty();
